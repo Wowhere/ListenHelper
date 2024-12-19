cab3bb2b3a718eeefabb36350b6580e626434a04
Andrew Malyshev <69252550+Wowhere@users.noreply.github.com>
2024-12-20 01:30:56 +0300

    update libs, thinking about exec

--- ViewModels/CodeWindowViewModel.cs
+++ ViewModels/CodeWindowViewModel.cs
@@ -9,6 +9,9 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using ReactiveUI;
 using voicio.Models;
 using System.IO;
+using System;
+using System.Linq;
+using System.Runtime.Loader;
 
 namespace voicio.ViewModels
 {
@@ -21,24 +24,44 @@ namespace voicio.ViewModels
             get => _TitleText;
             set => this.RaiseAndSetIfChanged(ref _TitleText, value);
         }
+        private string _ResultText = "";
+        public string ResultText
+        {
+            get => _ResultText;
+            set => this.RaiseAndSetIfChanged(ref _ResultText, value);
+        }
+        private string _CompileText = "";
+        public string CompileText
+        {
+            get => _CompileText;
+            set => this.RaiseAndSetIfChanged(ref _CompileText, value);
+        }
         private AvaloniaEdit.Document.TextDocument _SourceCode;
         public AvaloniaEdit.Document.TextDocument SourceCode
         {
             get => _SourceCode;
             set => this.RaiseAndSetIfChanged(ref _SourceCode, value);
         }
+        public void RunCode()
+        {
+            AssemblyLoadContext runContext = new AssemblyLoadContext("");
+            runContext.LoadFromStream(new MemoryStream(bindedObject.ActionTreeExpression));
+            //System.Runtime.
+            //var dll = runContext.LoadFromStream(new MemoryStream());
+        }
         public void Compile()
         {
-
+            bindedObject.SourceCode = SourceCode.Text;
             var options = new CSharpCompilationOptions((OutputKind)LanguageVersion.Latest);
             var syntaxTree = CSharpSyntaxTree.ParseText(bindedObject.SourceCode);
-
             var compilation = CSharpCompilation.Create("DynamicAssembly")
             .AddSyntaxTrees(syntaxTree)
             .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+            
             using (var ms = new MemoryStream())
             {
                 var result = compilation.Emit(ms);
+                if (result.Success == true) ResultText = "Builded without errors";
                 bindedObject.ActionTreeExpression = ms.ToArray();
                 if (bindedObject.IsSaved)
                 {
